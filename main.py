6a6bc02b4bc691f066307be119520b2ab8608ef5
Shiv Thakur <125086310+shivthakur007@users.noreply.github.com>
2026-03-11 00:18:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
+import streamlit as st
+import ast
+import operator
+import math
+
+st.markdown(
+    """
+    <h1 style='text-align: right; color: orange;'>
+    Calculator 💸
+    </h1>
+    """,
+    unsafe_allow_html=True
+)
+
+st.sidebar.title("Calculators 🏦")
+
+calculator = st.sidebar.selectbox(
+    "Choose Calculator",
+    ["Financial", "Simple", "Scientific"]
+)
 
