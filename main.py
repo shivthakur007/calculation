f38b828ad70f2b6cf04a163365dd1f9e1141d9fc
Shiv Thakur <125086310+shivthakur007@users.noreply.github.com>
2026-03-20 07:47:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import streamlit as st
-from finance_functions import simple_interest, compound_interest, compound_value, present_value, pv_casflows
+from finance_functions import simple_interest, compound_interest, compound_value, present_value, pv_cashflows
 from expression_evaluator import evaluate_expression
 
 
