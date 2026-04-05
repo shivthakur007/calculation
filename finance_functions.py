024d7f7f0d891f9c79e0b0efdbcb097b0977aab1
Shiv Thakur <125086310+shivthakur007@users.noreply.github.com>
2026-04-05 23:24:15 +0530

    Update finance_functions.py

--- finance_functions.py
+++ finance_functions.py
@@ -20,3 +20,18 @@ def pv_cashflows(cashflows, rate):
         for _, row in cashflows.iterrows()
     )
     return pv_total
+
+def present_value_annuity(C, r, n):
+    r = r / 12          # monthly rate
+    n = n * 12          # convert years to months
+    PV = C * (1 - (1 + r) ** (-n)) / r
+    return PV
+
+def calculate_emi(P, annual_rate, years):
+    r = annual_rate / 12
+    n = years * 12 
+    if r == 0:
+        return P / n
+    EMI = P * r * (1 + r) ** n / ((1 + r) ** n - 1)
+    return EMI
+
